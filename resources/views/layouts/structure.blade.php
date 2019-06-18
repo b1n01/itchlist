e5c92310c87f9009b9bcac8fc20c8880a08916cf
Luca Lorenzini <chielorenz@gmail.com>
2019-06-18 14:16:37 +0200

    UPDATE: open graph image

--- resources/views/layouts/structure.blade.php
+++ resources/views/layouts/structure.blade.php
@@ -32,7 +32,7 @@
 
     <!-- Open Graph -->
     <meta property="fb:app_id" content="{{ config('services.facebook.client_id') }}" />
-    <meta property="og:image" content="{{ asset('images/og-images.png') }}" />
+    <meta property="og:image" content="{{ asset('images/og-image.png') }}" />
     <meta property="og:title" content="Ithclist: a better wishlist" />
     <meta property="og:description" content="It's a platform that aims to ease the 'what can we gift?' problem. Just add your 'itches' to your list an share it with your dears/friends" />
     <meta property="og:type" content="website" />
