9986d7c3317c1d367feb2d89a62cdf41ea5c5162
Luca Lorenzini <chielorenz@gmail.com>
2019-07-06 19:48:04 +0200

    ADD: site description meta

--- resources/views/layouts/structure.blade.php
+++ resources/views/layouts/structure.blade.php
@@ -3,11 +3,12 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>{{ config('app.name', 'Ithclist') }}</title>
+    <meta name="description" content="Itchlist is a better wishlist. It lets you add your favourite Amazon items to your wish list and share it with your Facebook friends.">
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Ithclist') }}</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
